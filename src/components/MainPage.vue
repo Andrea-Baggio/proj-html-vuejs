7e25f4ad07b83b1c4fe7582914f115c8344a19e3
Andrea-Baggio <andreabaggio800@gmail.com>
2022-12-06 17:04:29 +0100

    array del main

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <div class="container2 mx-auto">
+      <!-- PRIMA PARTE -->
       <div class="box d-flex">
         <div class="box1">
           VIDEO
@@ -22,13 +23,26 @@
             <div class="kaixa fw-bolder">
               Kaixa Stark
             </div>
-
             <div class="name">
               WINGMAN
             </div>
           </div>
         </div>
       </div>
+      <!-- SECONDA PARTE -->
+      <ul class="py-5">
+        <li
+          v-for="element in arrPackages"
+          :key="element.id"
+        >
+          {{ element.price }}
+          <img
+            src="element.img"
+            alt=""
+            class="picture"
+          >
+        </li>
+      </ul>
     </div>
   </main>
 </template>
@@ -38,7 +52,43 @@ export default {
   name: 'MainPage',
   data() {
     return {
-
+      arrPackages: [{
+        img: '@/img/motivation-course-06-480x298.jpg',
+        price: '$30.00',
+        summa: 'How to be Successful: Create A Growth Mindset For Success',
+        nLessons: '3 Lessons',
+        nStudents: '50 Students',
+      },
+      {
+        price: '$30.00',
+        summa: 'How to Build Confidence in Your Abilities',
+        nLessons: '1 Lesson',
+        nStudents: '50 Students',
+      },
+      {
+        price: '$20.00',
+        summa: 'Productivity Machine - Focus in a Distracted World',
+        nLessons: '5 Lessons',
+        nStudents: '50 Students',
+      },
+      {
+        price: '$20.00',
+        summa: 'Effective Time Management Mastery - Complete Guide',
+        nLessons: '18 Lessons',
+        nStudents: '50 Students',
+      },
+      {
+        price: '$25.99',
+        summa: 'Body Language Secrets for Entrepreneurs',
+        nLessons: '19 Lessons',
+        nStudents: '50 Students',
+      },
+      {
+        price: '$19.99',
+        summa: 'Management Skills: The Science of Leadership',
+        nLessons: '17 Lessons',
+        nStudents: '50 Students',
+      }],
     };
   },
 };
@@ -62,11 +112,6 @@ export default {
   font-size: 1.1em;
 }
 
-// .founders-block {
-//   display: flex;
-//   flex-direction: column;
-// }
-
 .kaixa {
   color: rgb(66, 60, 101);
 }
@@ -74,4 +119,16 @@ export default {
 .name {
   font-size: .6em;
 }
+
+// SECONDA PARTE
+
+ul {
+  border: 2px solid red;
+}
+
+.picture {
+  height: 100px;
+  width: 100px;
+}
+
 </style>

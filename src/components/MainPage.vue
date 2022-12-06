480d1b2a038330298a2187fa3b10897d7d246481
Andrea-Baggio <andreabaggio800@gmail.com>
2022-12-06 15:38:29 +0100

    conclusione prima parte main (senza video)

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -2,12 +2,31 @@
   <main>
     <div class="container2 mx-auto">
       <div class="box d-flex">
-        <div class="box1 w-50">
+        <div class="box1">
           VIDEO
           <!-- TODO:video youtube -->
+          <!-- quando fai questa parte ricorda che puoi aggiustare le box con i padding -->
         </div>
-        <div class="box2 w-50">
-          hello
+        <div class="box2">
+          <div class="py-5">
+            "Every morning you have two <br> choices: continue to sleep with <br>
+            your dreams, or wake up and <br> chase them."
+          </div>
+          <div class="founders-block row d-flex flex-column gy-2">
+            <div>
+              <img
+                src="@/img/home-movation-shape-01.png"
+                alt=""
+              >
+            </div>
+            <div class="kaixa fw-bolder">
+              Kaixa Stark
+            </div>
+
+            <div class="name">
+              WINGMAN
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -34,8 +53,25 @@ export default {
   max-width: 1000px;
 }
 
-.box1,
+.box1 {
+  width: 65%;
+}
+
 .box2 {
-  border: 2px solid blue;
+  width: 35%;
+  font-size: 1.1em;
+}
+
+// .founders-block {
+//   display: flex;
+//   flex-direction: column;
+// }
+
+.kaixa {
+  color: rgb(66, 60, 101);
+}
+
+.name {
+  font-size: .6em;
 }
 </style>
